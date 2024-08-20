7826c4570ddd6df5a9b8d136ae7dda563ae027c3
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-20 10:08:47 +0300

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,23 @@ import {Logo} from "./Logo.jsx";
 import {Buttons} from "./Buttons.jsx";
 import {SpecialBtn} from "../SpecialBtn.jsx";
 import {Burger} from "./Burger.jsx";
+import {useRef} from "react";
+import useSmoothScroll from "../SmoothScroll.jsx";
+
 
 
 export const Header = () => {
+    const headerRef = useRef(null);
 
+    useSmoothScroll(headerRef);
     return (
-        <header className="page-container header">
-            <Logo/>
-            <Buttons/>
-            <SpecialBtn style={'special-button-green'}/>
-            <div className="bgr">
-                <Burger/>
+        <div className="page-container header" ref={headerRef}>
+            <div>
+                <Logo/>
+                <Buttons/>
+                <SpecialBtn style={'special-button-green'}/>
             </div>
-        </header>
+            <Burger/>
+        </div>
     )
 }
